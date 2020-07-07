f8a67943438fb905c0734676f584c76371a1305a
xurror <kaze.nasser@outlook.com>
2020-07-07 07:45:59 +0100

    log login error

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,45 +12,45 @@ use Exception;
 
 class AuthController extends Controller
 {
-    // /**
-    //  * Register a newly created resource in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function register(Request $request)
-    // {
-    //     $this->validate($request, [
-    //         'matricule' => 'required|string|min:5|max:15|unique:users',
-    //         'name' => 'required|string|max:255',
-    //         'email' => 'required|string|email|max:255|unique:users',
-    //         'password' => 'required|string|min:8',
-    //         'phone' => 'required|string|min:9|max:12',
-    //         'dob' => 'required|date',
-    //         'gender' => 'required',
-    //         'marital_status' => 'required',
-    //     ]);
+    /**
+     * Register a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'matricule' => 'required|string|min:5|max:15|unique:users',
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'phone' => 'required|string|min:9|max:12',
+            'dob' => 'required|date',
+            'gender' => 'required',
+            'marital_status' => 'required',
+        ]);
 
-    //     try {
-    //         $user = new User;
-    //         $user->matricule = $request['matricule'];
-    //         $user->name = $request['name'];
-    //         $user->email = $request['email'];
-    //         $user->password = Hash::make($request['password']);
-    //         $user->phone = $request['phone'];
-    //         $user->dob = $request['dob'];
-    //         $user->gender = $request['gender'];
-    //         $user->marital_status = $request['marital_status'];
-    //         $user->role = "admin";
-    //         $user->save();
-    //         //return successful response
-    //         return response()->json(['user' => $user, 'message' => 'new User Created'], 200);
+        try {
+            $user = new User;
+            $user->matricule = $request['matricule'];
+            $user->name = $request['name'];
+            $user->email = $request['email'];
+            $user->password = Hash::make($request['password']);
+            $user->phone = $request['phone'];
+            $user->dob = $request['dob'];
+            $user->gender = $request['gender'];
+            $user->marital_status = $request['marital_status'];
+            $user->role = "admin";
+            $user->save();
+            //return successful response
+            return response()->json(['user' => $user, 'message' => 'new User Created'], 200);
 
-    //     } catch (Exception $e) {
-    //         //return error message
-    //         return response()->json(['message' => 'User Registration Failed!'], 409);
-    //     }
-    // }
+        } catch (Exception $e) {
+            //return error message
+            return response()->json(['message' => 'User Registration Failed!'], 409);
+        }
+    }
 
     /**
      * Login a user.
@@ -67,26 +67,29 @@ class AuthController extends Controller
             'role' => 'required|string',
         ]);
 
-        if (filter_var($request->input('username'), FILTER_VALIDATE_EMAIL)) {
-            $type = 'email';
-            $user = DB::table('users')->where('email', $request->input('username'))->first();
-        }else {
-            $type = 'matricule';
-            $user = DB::table('users')->where('matricule', $request->input('username'))->first();
-        }
-
-        $request->merge([$type => $request->input('username')]);
-
-        // Verify the password and generate the token
-        if (Hash::check($request['password'], $user->password)) {
-            $credentials = $request->only([$type, 'password']);
-
-            if (!$token = Auth::attempt($credentials)) {
-                return response()->json(['message' => 'Unauthorized'], 401);
+        try {
+            if (filter_var($request->input('username'), FILTER_VALIDATE_EMAIL)) {
+                $type = 'email';
+                $user = User::where('email', $request->input('username'))->first();
+            }else {
+                $type = 'matricule';
+                $user = User::where('matricule', $request->input('username'))->first();
             }
 
-            return $this->respondWithToken($user, $token);
-        }
+            $request->merge([$type => $request->input('username')]);
 
+            // Verify the password and generate the token
+            if (Hash::check($request['password'], $user->password)) {
+                $credentials = $request->only([$type, 'password']);
+
+                if (!$token = Auth::attempt($credentials)) {
+                    return response()->json(['message' => 'Unauthorized'], 401);
+                }
+                return $this->respondWithToken($user, $token);
+            }
+        } catch (\Exception $e) {
+            error_log('An error occured' . $e);
+            return response()->json(['message' => 'User not found'], 404);
+        }
     }
 }
